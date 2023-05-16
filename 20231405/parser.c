7c6f82b5d1d72e3cf5585ca8f9893d20f9cb639d
Fiona Cahalan <fionamcahalan@gmail.com>
2023-05-16 17:09:39 -0400

    compiles, started adding operations, about to add tree

--- 20231405/parser.c
+++ 20231405/parser.c
@@ -10,11 +10,32 @@ struct node{
 struct opr{
 	struct node super;
 	int (*opr)(int, int);
+};
+
+struct num{
+	struct node super;
+	int number;
+};
+
+static int multiply(int a, int b){
+	return a * b;
+}
+
+static int divide(int a, int b){
+	return a / b;
+}
+
+static int add(int a, int b){
+	return a + b;
+}
+
+static int subtract(int a, int b){
+	return a - b;
 }
 
 static int c_idx = 0;
 static char next_char(void){
-	char equation[] = "124";
+	char equation[] = "124*34";
 	return equation[c_idx++];
 } 
 
@@ -23,9 +44,22 @@ int main(int argc, char *argv[]){
 	(void)argv;
 	int ch = next_char();
 	int number = 0;
-	while (ch) {
-		if (ch > 47 && ch < 58) {
+	while(ch) {
+		if(ch > 47 && ch < 58) {
 			number = number * 10 + (ch - 48);
+		} else {
+			printf("%d\n", number);
+			number = 0;
+			struct opr *temp = malloc(sizeof *temp);
+			if(ch == 42){
+				temp->opr = multiply;
+			} if(ch == 43){
+				temp->opr = add;
+			} if(ch == 45) {
+				temp->opr = subtract;
+			} if(ch == 47) {
+				temp->opr = divide;
+			}
 		}
 		ch = next_char();
 	}
