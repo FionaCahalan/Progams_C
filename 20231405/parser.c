e960781b46dc06e5a36d4e90ee8909d1e132349d
Fiona Cahalan <fionamcahalan@gmail.com>
2023-05-23 14:09:09 -0400

    Before trying to fix my mess

--- 20231405/parser.c
+++ 20231405/parser.c
@@ -3,14 +3,33 @@
 #include <stdlib.h>
 #include <math.h>
 
+struct node;
+
+struct node_vtable{
+	// void (*print)();
+    int (*evaluate)(int, int);
+};
+
 struct node{
-	struct node *children;
+	struct node *left_c;
+	struct node *right_c;
+	int type;
+    // 0 = number
+	// 1 = function
 };
 
 struct opr{
 	struct node super;
-	int (*opr)(int, int);
+    struct node_vtable *opr;
 };
+
+/*
+fix naming. 
+have operator store the left and right child
+Have node store the v_table and num store the number
+
+*/
+
 
 struct num{
 	struct node super;
@@ -33,11 +52,18 @@ static int subtract(int a, int b){
 	return a - b;
 }
 
+static struct node_vtable multi = {
+    // .print = 
+    .evaluate = multiply,
+};
+
 static int c_idx = 0;
 static char equation[] = "124*34";
+static struct node* symbol;
+
 static char next_char(void){
 	return equation[c_idx++];
-} 
+}
 
 static char peak_char(void){
 	return equation[c_idx];
@@ -46,81 +72,87 @@ static char peak_char(void){
 static struct node *next_part(void){
 	int ch = next_char();
 	// could probably remove if statement
-	if(ch && ch > 47 && ch < 58){
-		int number = ch - 48;
-		while(peak_char() && peak_char() > 47 && peak_char() < 58){
+	if(ch && ch >= '0' && ch <= '9'){
+		int number = ch - '0';
+		while(peak_char() && peak_char() >= '0' && peak_char() <= '9'){
 			ch = next_char();
-			number = number * 10 + (ch - 48);
+			number = number * 10 + (ch - '0');
 		}
-		struct num *tmp = malloc(sizeof *tmp);
+		struct num *tmp = calloc(sizeof *tmp, 1);
 		tmp->number = number;
+		tmp->super.type = 0;
 		printf("%d\n", number);
 		return &(tmp->super);
 	} else {
-		struct opr *tmp = malloc(sizeof *tmp);
-		if(ch == 42){
-			tmp->opr = multiply;
-		} if(ch == 43){
-			tmp->opr = add;
-		} if(ch == 45) {
-			tmp->opr = subtract;
-		} if(ch == 47) {
-			tmp->opr = divide;
+		struct opr *tmp = calloc(sizeof *tmp, 1);
+		if(ch == '*'){
+			tmp->opr = &multi;
+		} if(ch == '+'){
+			tmp->opr = &multi;
+		} if(ch == '-') {
+			tmp->opr = &multi;
+		} if(ch == '/') {
+			tmp->opr = &multi;
 		}
+		tmp->super.type = 1;
 		printf("%s\n", "OP");
-		return &(tmp->super);
+		return &tmp->super;
 	}
-	/*
-	int number = 0;
-	while(ch) {
-		if(ch > 47 && ch < 58) {
-			number = number * 10 + (ch - 48);
-		} else {
-			printf("%d\n", number);
-			number = 0;
-			struct opr *temp = malloc(sizeof *temp);
-			if(ch == 42){
-				temp->opr = multiply;
-			} if(ch == 43){
-				temp->opr = add;
-			} if(ch == 45) {
-				temp->opr = subtract;
-			} if(ch == 47) {
-				temp->opr = divide;
-			}
-		}
-		ch = next_char();
-	}
-	*/
+}
+
+static struct node *factor(void){
+    if (symbol == num) {
+        struct node *temp = symbol;
+        symbol = next_part();
+        return temp;
+    }
+}
+
+static struct node *term(void){
+    struct node *head = factor();
+    while (peak_char() && symbol. ==  * /){
+        symbol->left_c = head;
+        head = symbol;
+        symbol = next_part();
+        head->right_c = factor();
+    }
+    return head;
+}
+
+static struct node *expression(void){
+    struct node *head = term();
+    while (peak_char() && symbol->type == 1 && (symbol+-)){
+        symbol->left_c = head;
+        head = symbol;
+        symbol = next_part();
+        head->right_c = term();
+    }
+    return head;
 }
 
 int main(int argc, char *argv[]){
 	(void)argc;
 	(void)argv;
-	while(peak_char())
-		next_part();
+	if(peak_char()){
+        symbol = next_part();
+        expression();
+	}
 }
 
 /*
-Possible approaches:
+Expression
+term {("+"|"-") term}
 
-- Split the lexer and parser
-- Recursion
-- use the tree to simulate recursion
+term
+factor {("*"|"/") factor}
 
-Add part
-if node is non-delimiter,
-- if node doesn't have two children, call recursively on right child, left child should already be made?
-- else call recursively, creating parent?
-
-if delimiter
-- next part is the parent of the delimiter, UNLESS delimeter is right child (then return).
-
+factor
+number
 
 
 Ex: 142 + 36
 
-	+
+	    +
        / \
      142  36
 */
